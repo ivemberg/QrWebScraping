c7158ceac3d6c7e76df02efd8da4960d5c4b35ab
i.bettini <i.bettini@gmail.com>
2022-06-24 15:51:44 +0200

    Initial Commit

---
+++ QrWebScraping/QrWebScrapingApp.swift
@@ -0,0 +1,17 @@
+//
+//  QrWebScrapingApp.swift
+//  QrWebScraping
+//
+//  Created by Ivo Junior Bettini on 24/06/22.
+//
+
+import SwiftUI
+
+@main
+struct QrWebScrapingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
