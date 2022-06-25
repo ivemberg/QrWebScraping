b485a9d71f8810138f1e8805dad6bef45744eea1
i.bettini <i.bettini@gmail.com>
2022-06-25 18:06:27 +0200

    update

--- QrWebScraping/ContentView.swift
+++ QrWebScraping/ContentView.swift
@@ -8,51 +8,100 @@
 import SwiftUI
 import SwiftSoup
 import Foundation
+import UIKit
+import WebKit
 
 struct ContentView: View {
     
-    @State var titolo = ""
-    @State var url = ""
+    @State var qrCode: Image = Image("")
+    @State var tito = ""
+    @State var newtito = ""
+    
     
     var body: some View {
-        NavigationView{
+        
+        Button("Generate Link") {
+            let url = URL(string: "https://it.wikipedia.org/wiki/Speciale:PaginaCasuale")!
+            let html = try! String(contentsOf: url)
+            let document = try! SwiftSoup.parse(html)
+            tito = try! document.title()
             
-            VStack{
-                Button("Generate Link") {
-                    let url = URL(string: "https://it.wikipedia.org/wiki/Speciale:PaginaCasuale")!
-                    let html = try! String(contentsOf: url)
-                    let document = try! SwiftSoup.parse(html)
-                    titolo = try! document.title()
-                    print(titolo)
-                }.padding()
-                
-                Text(titolo).padding()
-                Button("Generate QR") {
-                    let newTitolo: () = getTitolo()
-                    
-                    NavigationLink("\(newTitolo)" .capitalized, destination: VStack{
-                        Text("aaaa")
-                        }
-                    )
-                    
-                }.padding()
-            }
+        }.padding()
+        
+        
+        Text(tito).padding()
+        
+        
+        qrCode
+            .resizable()
+            .scaledToFit()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 200, height: 200)
+            .padding()
+        
+        
+        
+        Button("Genera QR Code") {
+            newtito = getTitolo()
+            qrCode = Image(uiImage: UIImage(data: getQRCodeDate(text: "https://it.wikipedia.org/wiki/\(newtito)")!)!)
+            
         }
-}
+    }
     
-func getTitolo(){
+    func getTitolo() -> String{
         
         var i = 0
         while i < 11{
-            titolo.removeLast()
+            tito.removeLast()
             i+=1
         }
         
+        return tito
+        
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+func pageParser(_ htmlPage: String) -> [(String, Int, String)] {
+    var output: [(String, Int, String)] = []
+    let splitted = htmlPage.split(separator: "\n")
+    var counterTitle: Int = 0
+    var counterParagraph: Int = 0
+    
+    for i in 0..<splitted.count {
+        if splitted[i].contains("<title>") {
+            output.append(("title",
+                           counterTitle,
+                           String(splitted[i].suffix(splitted[i].count - 7).prefix(splitted[i].count - 27))))
+            counterTitle += 1
+        }
+        
+        if splitted[i].contains("<p>") {
+            output.append(("p",
+                           counterParagraph,
+                           String(splitted[i])))
+            counterParagraph += 1
+        }
+    }
+    
+    print("Output: \(output)")
+    
+    return output
+}
+
+func getQRCodeDate(text: String) -> Data? {
+    guard let filter = CIFilter(name: "CIQRCodeGenerator") else { return nil }
+    let data = text.data(using: .ascii, allowLossyConversion: false)
+    filter.setValue(data, forKey: "inputMessage")
+    guard let ciimage = filter.outputImage else { return nil }
+    let transform = CGAffineTransform(scaleX: 10, y: 10)
+    let scaledCIImage = ciimage.transformed(by: transform)
+    let uiimage = UIImage(ciImage: scaledCIImage)
+    return uiimage.pngData()!
 }
