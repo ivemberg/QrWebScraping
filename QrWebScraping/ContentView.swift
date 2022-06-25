bc6620c0d596dd80cd5d12fe8468f5102c869b62
i.bettini <i.bettini@gmail.com>
2022-06-25 17:05:47 +0200

    update

--- QrWebScraping/ContentView.swift
+++ QrWebScraping/ContentView.swift
@@ -6,11 +6,48 @@
 //
 
 import SwiftUI
+import SwiftSoup
+import Foundation
 
 struct ContentView: View {
+    
+    @State var titolo = ""
+    @State var url = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            
+            VStack{
+                Button("Generate Link") {
+                    let url = URL(string: "https://it.wikipedia.org/wiki/Speciale:PaginaCasuale")!
+                    let html = try! String(contentsOf: url)
+                    let document = try! SwiftSoup.parse(html)
+                    titolo = try! document.title()
+                    print(titolo)
+                }.padding()
+                
+                Text(titolo).padding()
+                Button("Generate QR") {
+                    let newTitolo: () = getTitolo()
+                    
+                    NavigationLink("\(newTitolo)" .capitalized, destination: VStack{
+                        Text("aaaa")
+                        }
+                    )
+                    
+                }.padding()
+            }
+        }
+}
+    
+func getTitolo(){
+        
+        var i = 0
+        while i < 11{
+            titolo.removeLast()
+            i+=1
+        }
+        
     }
 }
 
---
+++ QrWebScraping/WebScraping.swift
@@ -0,0 +1,11 @@
+//
+//  WebScraping.swift
+//  QrWebScraping
+//
+//  Created by Ivo Junior Bettini on 24/06/22.
+//
+
+import Foundation
+import SwiftSoup
+
+
